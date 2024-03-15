badd241f02125aea17fd3a2b339023756a86f9dd
KuranosukeOhta <ohta.kura@gmail.com>
2024-03-15 18:10:47 +0900

    Station5完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
+
+/*async function getDogImgData() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const imgData = await response.json();
+  return imgData
+}
+*/
+
 export const App = () => {
+  // 犬の画像を取得
+  const [dogUrl, setDogUrl] = useState('') //useStateの初期値
+
+  const fetchDogImg = async () => {
+    setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ")
+  }
+
+  function handleClick() {
+    fetchDogImg()
+    //alert("you clicked!")
+  }
+
+  // 取得した画像と併せてHTMLを構築
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>
+        <h1>Dogアプリ</h1>
+        <p>犬の画像を表示するサイトです</p>
+        <button onClick={handleClick}>更新</button>
+        <p></p>
+        <img src={dogUrl} />
+        <p>上部に画像が表示されます。</p>
+      </header>
+    </>
   )
 }
